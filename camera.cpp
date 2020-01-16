f3b803b2d452673890911f5fad8bf0bd38fa3d48
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-01-16 01:28:18 +0100

    Refactored camera class

--- camera.cpp
+++ camera.cpp
@@ -1,21 +1,19 @@
 #include "camera.h"
 
 // setup camera
-Camera::Camera(Vec cameraPosition, Vec CameraLookTo, double verticalFOV, double aspect) {
-    // vfov is top to bottom in degrees
-    Vec u, v, w; //defined in the figure 11-3
-    float theta = verticalFOV*M_PI/180;
+Camera::Camera(Vec cameraPosition, Vec CameraLookTo, double verticalFOV, double aspectWidthHeight) {
+    float theta = verticalFOV*M_PI/180; // Field of view is top to bottom in degrees
     float half_height = tan(theta/2);
-    float half_width = aspect * half_height;
+    float half_width = aspectWidthHeight * half_height;
     origin = cameraPosition;
-    w = unit_vector(cameraPosition - CameraLookTo);
-    u = unit_vector(cross(Vec(0,1,0), w));
-    v = cross(w, u);
+    Vec w = unit_vector(cameraPosition - CameraLookTo);
+    Vec u = unit_vector(cross(Vec(0,1,0), w));
+    Vec v = cross(w, u);
     lower_left_corner = origin - half_width*u - half_height*v - w;
     horizontal = 2*half_width*u;
     vertical = 2*half_height*v;
 }
 
-Ray Camera::get_ray(double u, double v) {
+Ray Camera::getRay(double u, double v) {
     return Ray(origin, lower_left_corner + horizontal*u + vertical*v - origin);
 }
--- camera.h
+++ camera.h
@@ -5,15 +5,15 @@
 
 class Camera {
     public:
+        Camera(){}
+        Camera(Vec cameraPosition, Vec CameraLookTo, double verticalFOV, double aspectWidthHeight);
+        Ray getRay(double u, double v);
+    private:
         Vec origin;
         Vec lower_left_corner;
         Vec horizontal;
         Vec vertical;
 
-        // set up camera
-        Camera(){}
-        Camera(Vec cameraPosition, Vec CameraLookTo, double verticalFOV, double aspect);
-        Ray get_ray(double u, double v);
 };
 
 #endif
