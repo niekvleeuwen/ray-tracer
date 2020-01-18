0009c10a17196cf866b4f0705ac7bba6ecfa42c6
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-01-18 15:30:29 +0100

    Updated camera class

--- camera.cpp
+++ camera.cpp
@@ -1,10 +1,10 @@
 #include "camera.h"
 
 // Setup camera
-Camera::Camera(Vector3D cameraPosition, Vector3D CameraLookTo, double verticalFOV, double aspectWidthHeight) {
+Camera::Camera(Vector3D cameraPosition, Vector3D CameraLookTo, double verticalFOV, double ratioWidthHeight) {
     float theta = verticalFOV*M_PI/180; // Field of view is top to bottom in degrees
     float half_height = tan(theta/2);
-    float half_width = aspectWidthHeight * half_height;
+    float half_width = ratioWidthHeight * half_height;
     origin = cameraPosition;
     Vector3D w = unit_vector(cameraPosition - CameraLookTo);
     Vector3D u = unit_vector(cross(Vector3D(0,1,0), w));
@@ -14,6 +14,7 @@ Camera::Camera(Vector3D cameraPosition, Vector3D CameraLookTo, double verticalFO
     vertical = 2*half_height*v;
 }
 
-Ray Camera::getRay(double u, double v) {
-    return Ray(origin, lower_left_corner + horizontal*u + vertical*v - origin);
+// This function returns the ray at (x,y)
+Ray Camera::getRay(double x, double y) {
+    return Ray(origin, lower_left_corner + horizontal*x + vertical*y - origin);
 }
--- camera.h
+++ camera.h
@@ -4,8 +4,8 @@
 class Camera {
     public:
         Camera(){}
-        Camera(Vector3D cameraPosition, Vector3D CameraLookTo, double verticalFOV, double aspectWidthHeight);
-        Ray getRay(double u, double v);
+        Camera(Vector3D cameraPosition, Vector3D CameraLookTo, double verticalFOV, double ratioWidthHeight);
+        Ray getRay(double x, double y);
     private:
         Vector3D origin;
         Vector3D lower_left_corner;
